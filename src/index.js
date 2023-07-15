077697106d2522b5332623a537ebabc6c22c9625
devlsy86 <infightersy@naver.com>
2023-07-15 21:29:06 +0900

    이제 react redux 도구 사용할 차례(강좌 5-8)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
+import { createStore } from 'redux';
+import reducers from './reducers';  
+
+import * as actions from './actions';
+
+const store = createStore(reducers);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
